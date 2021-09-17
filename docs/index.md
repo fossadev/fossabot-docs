b7b279e053f36fbf5665c3191fb1407240c7d5a9
Aiden Wallis <aiden@aidenwallis.co.uk>
2021-09-17 23:39:26 +0100

    initial commit

---
+++ docs/index.md
@@ -0,0 +1,10 @@
+---
+id: welcome
+slug: /
+sidebar_position: 1
+---
+# Welcome to Fossabot
+
+Fossabot is a Twitch chat bot that has all the features you need to create the ultimate chat experience for yourself and your audience. Built by the community, for the community.
+
+This documentation aims to help familiarize you with the many features Fossabot provides, and show you how to make the most out of your chat bot.
---
+++ docusaurus.config.js
@@ -0,0 +1,108 @@
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+// With JSDoc @type annotations, IDEs can provide config autocompletion
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+(module.exports = {
+  title: 'Fossabot',
+  tagline: 'Dinosaurs are cool',
+  url: 'https://docs.fossabot.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'fossadev', // Usually your GitHub org/user name.
+  projectName: 'fossabot-docs', // Usually your repo name.
+
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl: 'https://github.com/fossadev/fossabot-docs/edit/main/website/',
+          routeBasePath: '/',
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: '',
+        logo: {
+          alt: 'Fossabot',
+          src: 'img/logo.svg',
+        },
+        items: [
+          // {
+          //   type: 'doc',
+          //   docId: 'welcome',
+          //   position: 'left',
+          //   label: 'Tutorial',
+          // },
+          {
+            href: 'https://github.com/fossadev/fossabot-docs',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Documentation',
+                to: '/',
+              },
+            ],
+          },
+          {
+            title: 'Stay Connected',
+            items: [
+              {
+                label: 'Twitch',
+                href: 'https://twitch.tv/Fossabot',
+              },
+              {
+                label: 'Discord',
+                href: 'https://fossabot.com/discord',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/Fossabot',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Terms of Serivce',
+                to: 'https://fossadev.com/tos',
+              },
+              {
+                label: 'Privacy Policy',
+                href: 'https://fossadev.com/privacy',
+              },
+            ],
+          },
+        ],
+        copyright: `© ${new Date().getFullYear()}, FossaDev Ltd. All rights reserved. Registered in England and Wales (No. 12219499).`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+});
